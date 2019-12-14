581c8cddeb4c72e5f188c663a8b680f63ac1130c
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2019-12-14 02:56:21 +0100

    Day1.1: Calculate required fuel - Read from file

--- src/main/java/se/johannesdahlgren/adventofcode/y2019/Day1.java
+++ src/main/java/se/johannesdahlgren/adventofcode/y2019/Day1.java
@@ -1,16 +1,37 @@
 package se.johannesdahlgren.adventofcode.y2019;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Day1 {
 
   public int calculateRequiredFuelForModuleMass(int moduleMass) {
     return (int) (Math.floor(moduleMass / 3.0f) - 2);
   }
 
-  public int calculateRequiredFuelForModuleMass(int... moduleMass) {
+  public int calculateRequiredFuelForModuleMass(List<Integer> moduleMass) {
     int requiredFuel = 0;
     for (int mass : moduleMass) {
       requiredFuel += calculateRequiredFuelForModuleMass(mass);
     }
     return requiredFuel;
+  }
+
+  public int calculateRequiredFuelForModuleMass(String filePath) throws IOException, URISyntaxException {
+    URL fileUrl = this.getClass().getClassLoader().getResource(filePath);
+    if (fileUrl == null) {
+      return 0;
+    }
+
+    List<Integer> massForAllModules = Files.readAllLines(Paths.get(fileUrl.toURI()))
+        .stream()
+        .map(Integer::parseInt)
+        .collect(Collectors.toList());
+    return calculateRequiredFuelForModuleMass(massForAllModules);
   }
 }
--- src/test/java/se/johannesdahlgren/adventofcode/y2019/Day1Test.java
+++ src/test/java/se/johannesdahlgren/adventofcode/y2019/Day1Test.java
@@ -3,6 +3,9 @@ package se.johannesdahlgren.adventofcode.y2019;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -49,8 +52,23 @@ class Day1Test {
     int module2Mass = 14;
     int module3Mass = 1969;
     int module4Mass = 100756;
-    int requiredFuel = day1.calculateRequiredFuelForModuleMass(module1Mass, module2Mass, module3Mass, module4Mass);
+
+    int requiredFuel = day1
+        .calculateRequiredFuelForModuleMass(List.of(module1Mass, module2Mass, module3Mass, module4Mass));
     assertThat(requiredFuel, is(2 + 2 + 654 + 33583));
   }
 
+  @Test
+  void massOfAllModulesWithExampleInput() throws IOException, URISyntaxException {
+    String filePath = "day1_1_examples.txt";
+    int requiredFuel = day1.calculateRequiredFuelForModuleMass(filePath);
+    assertThat(requiredFuel, is(2 + 2 + 654 + 33583));
+  }
+
+  @Test
+  void massOfAllModulesWithRealInput() throws IOException, URISyntaxException {
+    String filePath = "day1_1.txt";
+    int requiredFuel = day1.calculateRequiredFuelForModuleMass(filePath);
+    assertThat(requiredFuel, is(3342946));
+  }
 }
