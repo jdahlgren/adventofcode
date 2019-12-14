8d34b56c3cb1a4b5241028044acdb94e7352db7a
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2019-12-14 10:01:45 +0100

    Day1.2: Reorder methods

--- src/main/java/se/johannesdahlgren/adventofcode/y2019/Day1.java
+++ src/main/java/se/johannesdahlgren/adventofcode/y2019/Day1.java
@@ -10,33 +10,9 @@ import java.util.stream.Collectors;
 
 public class Day1 {
 
-  private int calculateRequiredFuelForMass(int mass) {
-    return (int) (Math.floor(mass / 3.0f) - 2);
-  }
-
-  private int calculateRequiredFuelForMass(List<Integer> masses) {
-    return masses.stream()
-        .map(this::calculateRequiredFuelForMass)
-        .mapToInt(value -> value).sum();
-  }
-
   public int calculateRequiredFuelForMass(String filePath) {
     List<Integer> masses = getModuleMassFromFile(filePath);
     return calculateRequiredFuelForMass(masses);
-  }
-
-  private int calculateRequiredFuelForMassOfModuleAndItsFuel(int moduleMass) {
-    int requiredFuel = calculateRequiredFuelForMass(moduleMass);
-    if (requiredFuel <= 0) {
-      return 0;
-    }
-    return requiredFuel + calculateRequiredFuelForMassOfModuleAndItsFuel(requiredFuel);
-  }
-
-  private int calculateRequiredFuelForMassOfModuleAndItsFuel(List<Integer> masses) {
-    return masses.stream()
-        .map(this::calculateRequiredFuelForMassOfModuleAndItsFuel)
-        .mapToInt(value -> value).sum();
   }
 
   public int calculateRequiredFuelForMassOfModuleAndItsFuel(String filePath) {
@@ -58,5 +34,29 @@ public class Day1 {
     } catch (IOException | URISyntaxException e) {
       return List.of();
     }
+  }
+
+  private int calculateRequiredFuelForMass(List<Integer> masses) {
+    return masses.stream()
+        .map(this::calculateRequiredFuelForMass)
+        .mapToInt(value -> value).sum();
+  }
+
+  private int calculateRequiredFuelForMass(int mass) {
+    return (int) (Math.floor(mass / 3.0f) - 2);
+  }
+
+  private int calculateRequiredFuelForMassOfModuleAndItsFuel(List<Integer> masses) {
+    return masses.stream()
+        .map(this::calculateRequiredFuelForMassOfModuleAndItsFuel)
+        .mapToInt(value -> value).sum();
+  }
+
+  private int calculateRequiredFuelForMassOfModuleAndItsFuel(int moduleMass) {
+    int requiredFuel = calculateRequiredFuelForMass(moduleMass);
+    if (requiredFuel <= 0) {
+      return 0;
+    }
+    return requiredFuel + calculateRequiredFuelForMassOfModuleAndItsFuel(requiredFuel);
   }
 }
