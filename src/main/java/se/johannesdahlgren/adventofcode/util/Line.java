0ba2150716e058e36bf26746ffc47cc4c1d9011a
Johannes Dahlgren <johannes.dahlgren@gmail.com>
2019-12-14 22:29:01 +0100

    Day3.1: Setup the lines

--- src/main/java/se/johannesdahlgren/adventofcode/util/Line.java
+++ src/main/java/se/johannesdahlgren/adventofcode/util/Line.java
@@ -1,16 +1,21 @@
 package se.johannesdahlgren.adventofcode.util;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Line {
 
-  private List<Point> line;
-
-  public Line(List<Point> line) {
-    this.line = line;
-  }
+  private List<Point> line = new ArrayList<>();
 
   public List<Point> getLine() {
     return line;
+  }
+
+  public void addPoint(Point p) {
+    line.add(p);
+  }
+
+  public Point getLastPoint() {
+    return line.get(line.size() - 1);
   }
 }
--- src/main/java/se/johannesdahlgren/adventofcode/y2019/Day3.java
+++ src/main/java/se/johannesdahlgren/adventofcode/y2019/Day3.java
@@ -1,15 +1,70 @@
 package se.johannesdahlgren.adventofcode.y2019;
 
+import java.util.ArrayList;
 import java.util.List;
 import se.johannesdahlgren.adventofcode.util.FileToListUtil;
+import se.johannesdahlgren.adventofcode.util.Line;
+import se.johannesdahlgren.adventofcode.util.Point;
 
 public class Day3 {
 
+  private static final String RIGHT = "R";
+  private static final String UP = "U";
+  private static final String LEFT = "L";
+  private static final String DOWN = "D";
+
+  private final List<String> pathCommandsForLines;
+  private List<Line> lines;
+
   public Day3(String filePath) {
-    List<String> pathCommands = FileToListUtil.getLinePathFromFile(filePath);
+    pathCommandsForLines = FileToListUtil.getLinePathFromFile(filePath);
+    lines = new ArrayList<>();
   }
 
   public int findDistanceToClosestIntersection() {
+    convertPathCommandsToLines();
     return 0;
+  }
+
+  private void convertPathCommandsToLines() {
+    for (String pathCommandsForLine : pathCommandsForLines) {
+      createLine(pathCommandsForLine);
+    }
+  }
+
+  private void createLine(String pathCommandsForLine) {
+    Line line = new Line();
+    Point start = new Point(0, 0);
+    line.addPoint(start);
+
+    String[] pathCommand = pathCommandsForLine.split(",");
+    for (String step : pathCommand) {
+      createPointFromStep(step, line);
+    }
+    lines.add(line);
+  }
+
+  private void createPointFromStep(String step, Line line) {
+    String direction = step.substring(0, 1);
+    int amount = Integer.parseInt(step.substring(1));
+    for (int i = 0; i < amount; i++) {
+      Point lastPoint = line.getLastPoint();
+      Point nextPoint = getNewPointInDirection(direction, lastPoint);
+      line.addPoint(nextPoint);
+    }
+  }
+
+  private Point getNewPointInDirection(String direction, Point lastPoint) {
+    switch (direction) {
+      case UP:
+        return new Point(lastPoint.getX(), lastPoint.getY() + 1);
+      case RIGHT:
+        return new Point(lastPoint.getX() + 1, lastPoint.getY());
+      case DOWN:
+        return new Point(lastPoint.getX(), lastPoint.getY() - 1);
+      case LEFT:
+        return new Point(lastPoint.getX() - 1, lastPoint.getY());
+    }
+    throw new RuntimeException("Unknown direction");
   }
 }
